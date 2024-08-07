cbe6722e162a253e0bc72482674488343befb456
Khushi Allawadi <113546055+Khushi-Allawadi@users.noreply.github.com>
2024-08-07 12:33:05 +0530

    Create Day-0.java

---
+++ Day-0.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       String inputString = sc.nextLine();
+       System.out.println("Hello, World.\n"+ inputString);
+    }
+}
