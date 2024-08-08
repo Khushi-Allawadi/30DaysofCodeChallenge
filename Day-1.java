b0b7f1a1091e5ae2f30ffd607710d7b3da53f186
Khushi Allawadi <113546055+Khushi-Allawadi@users.noreply.github.com>
2024-08-08 13:00:02 +0530

    Update Day-1.java

--- Day-1.java
+++ Day-1.java
@@ -1,1 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       int i = 4;
+       double d = 4.0;
+       String s = "HackerRank ";
+       
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       double b = sc.nextDouble();
+       sc.nextLine();
+       String str = sc.nextLine();
+       
+       System.out.println(a + i);
+       System.out.println(b + d);
+       System.out.println(s + str);
+              
+    }
+}
 
